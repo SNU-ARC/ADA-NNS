23aad7abe83df1252b0e07b0fbcb8ec1a0a27598
k16diablo <miguel92@naver.com>
2025-02-17 20:53:54 +0900

    Not used

--- init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git submodule update --init efanna_graph
-git submodule update --init nsg
-git submodule update --init SSG
-git submodule update --init DiskANN
-git submodule update --init TOGG
